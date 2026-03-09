89bf9c191bf3505e0f959e7b8725cf22a2116d4e
Shiva Bhusal <srollins175@gmail.com>
2026-03-10 00:25:23 +0545

    Auth

--- AI-powered-full-stack/src/app.js
+++ AI-powered-full-stack/src/app.js
@@ -4,7 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+// Require all the route here
+const authRouter = require('./routes/auth.route');
 
-
+// Using All the Routes Here
+app.use('/api/auth', authRouter);
 
 module.exports = app;
---
+++ AI-powered-full-stack/src/controllers/auth.controllers.js
@@ -0,0 +1,15 @@
+const userModel = require("../models/user.model");
+
+/**
+ * @route POST /api/auth/register
+ * @desc Register a new user, excepts username, email and password in the request body
+ * @access Public
+ */
+
+async function registerUserController(req, res) {
+  
+}
+
+module.exports = {
+  registerUserController
+};
