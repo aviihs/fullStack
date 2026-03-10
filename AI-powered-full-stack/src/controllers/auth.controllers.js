a6b27224d551daa61695e1b747845f9cf406db9d
Shiva Bhusal <srollins175@gmail.com>
2026-03-10 10:25:52 +0545

    Login&Register AUTH

--- AI-powered-full-stack/src/controllers/auth.controllers.js
+++ AI-powered-full-stack/src/controllers/auth.controllers.js
@@ -1,15 +1,107 @@
-const userModel = require("../models/user.model");
+const userModel = require("../model/user.model.js");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 /**
- * @route POST /api/auth/register
+ * @name registerUserController
  * @desc Register a new user, excepts username, email and password in the request body
  * @access Public
  */
 
 async function registerUserController(req, res) {
-  
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({
+      message: "Please! provide username, email or password",
+    });
+  }
+  const isUserAlreadyExists = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (isUserAlreadyExists) {
+    return res.status(400).json({
+      message: "Username or Email Address Already Existed!!!",
+    });
+  }
+
+  const hash = await bcrypt.hash(password, 10);
+
+  const user = await userModel.create({
+    username,
+    email,
+    password: hash,
+  });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      username: user.username,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    },
+  );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "New User Registered Sucessfully",
+    user: {
+      id: user._id,
+      email: user.email,
+      username: user.username,
+    },
+  });
+}
+
+/**
+ * @name loginUserController
+ * @desc login a user, expects emails and passwords in request body
+ * @access Public
+ */
+
+async function loginUserController(req, res) {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(400).json({
+      message: "Invalid Email Address.",
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      message: "Incorrect Password.",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      username: user.username,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    },
+  );
+
+  res.cookie("token", token);
+  res.status(200).json({
+    message: "User Logged in Successfully",
+    user: {
+      id: user._id,
+      email: user.email,
+      username: user.username,
+    },
+  });
 }
 
 module.exports = {
-  registerUserController
+  registerUserController,
+  loginUserController,
 };
