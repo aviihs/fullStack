ebe5f49dc2f18873fed0d16c163d4488489efbd6
Shiva Bhusal <srollins175@gmail.com>
2026-03-10 10:27:10 +0545

    Dotenv Onfig

--- AI-powered-full-stack/src/server.js
+++ AI-powered-full-stack/src/server.js
@@ -1,13 +1,14 @@
-require("dotenv").config({path: "../.env"});
-const app = require('./app');
-const connectToDB = require('./config/database');
+require("dotenv").config();
+const app = require("./app");
+const connectToDB = require("./config/database");
 
 connectToDB();
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
+
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
